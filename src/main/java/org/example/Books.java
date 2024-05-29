0130b84101c46482572cadb24753873a6bfc12ea
MarcoSkiadas <Skiadas2@hotmail.com>
2024-05-29 15:56:04 +0200

    Adding Books Class

---
+++ src/main/java/org/example/Books.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class Books {
+
+    private String isbn;
+    private String title;
+    private String author;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
